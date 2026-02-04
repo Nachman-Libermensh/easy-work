adc9233fc5bdb4f37f8c30b75fcdf59d5fa1e413
Nachman-Libermensh <bn8475266@gmail.com>
2026-02-05 01:59:50 +0200

    feat: update deployment workflow and configure base path for Next.js site

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  // הוספת השורה הזו היא הקריטית כדי שהאתר יעבוד תחת הנתיב הספציפי
+  basePath: "/easy-work",
   images: {
     unoptimized: true,
   },
